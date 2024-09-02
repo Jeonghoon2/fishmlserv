66fe5adde6a9074a430a01ac99ca68db5f551b18
Jeonghoon2 <ukidd12@naver.com>
2024-09-02 20:05:50 +0900

    fix load_csv

--- src/fishmlserv_git/ml/ml.py
+++ src/fishmlserv_git/ml/ml.py
@@ -2,11 +2,17 @@ import pandas as pd
 
 # 읽어야 할 csv file이 있는지 없는 지 검사
 def load_csv(path : str):
-
     fish_data = pd.read_csv(path)
+    partition_cols = ["Label"]
+    fish_data.to_parquet(
+        "~/.fish_data/", 
+        partition_cols=partition_cols,
+        index=False
+        )
+    
     return fish_data.head(5)
 
-# 물고기 별로 Partition 후 Parquet 형태로 저장
+
 def predict():
     ...
 
