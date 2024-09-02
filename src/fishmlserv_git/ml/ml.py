1f15a7ed5fea9d9ef3ba83129b8e68e9e0c00eb0
Jeonghoon2 <ukidd12@naver.com>
2024-09-02 19:54:28 +0900

    feat read_csv

---
+++ src/fishmlserv_git/ml/ml.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 읽어야 할 csv file이 있는지 없는 지 검사
+def load_csv(path : str):
+    fish_data = pd.read_csv(path)
+    return fish_data.head(5)
+
+# 물고기 별로 Partition 후 Parquet 형태로 저장
+def predict():
+    ...
+
+def agg():
+    ...
